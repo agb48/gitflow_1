17edbc45f5970f61ce10c7ac3d56e5ade4716c7f
German Barros <gbarros@3cinteractive.com>
2014-05-22 11:07:32 -0300

    sdfgsdfgsd

--- free_proj/src/main/java/com/T.java
+++ free_proj/src/main/java/com/T.java
@@ -9,6 +9,7 @@ public class T {
 
 
 		System.out.println("1");
+		System.out.println("dev");
 		System.out.println("cambiowerwerwerwr en redfdfdflease 1");
 		System.out.println("feature-1");
 		System.out.println("bug");
