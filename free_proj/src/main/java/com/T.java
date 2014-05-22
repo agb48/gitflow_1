18dbf109bbe166c786a0f666e5fa75ad6c408c22
German Barros <gbarros@3cinteractive.com>
2014-05-22 16:25:27 -0300

    dfgh

--- free_proj/src/main/java/com/T.java
+++ free_proj/src/main/java/com/T.java
@@ -6,13 +6,6 @@ public class T {
 	public T(){}
 
 	public void doit(){
-
-
-		System.out.println("1");
-		System.out.println("dev");
-		System.out.println("cambiowerwerwerwr en redfdfdflease 1");
-		System.out.println("feature-1");
-		System.out.println("bug");
-		System.out.println("dos");
+		System.out.println("cambio para el release");
 	}
 }
